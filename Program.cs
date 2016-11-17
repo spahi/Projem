2cba2d46568a7df6cc0c4dce51e9fd6183611770
spahi <spahiyahya@outlook.com>
2016-11-17 14:14:38 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Merhaba dunya  merhaba kosova!");
+            Console.WriteLine("Merhaba dunya  merhaba kosova! devami");
+            Console.Readeline();
         }
     }
 }
