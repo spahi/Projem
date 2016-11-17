8aa20e052d39b9401fdba02925075ac8600173a7
spahi <spahiyahya@outlook.com>
2016-11-17 14:11:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba dunya  merhaba kosova!");
+        }
+    }
+}
